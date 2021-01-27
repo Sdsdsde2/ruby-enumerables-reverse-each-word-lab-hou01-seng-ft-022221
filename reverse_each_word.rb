2b221660fd9b363ea46a040d92ff423cef2399d1
Shane R <imspammails@gmail.com>
2021-01-27 01:16:23 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  new_sentence = ""
+  #array.each do |word|
+   # # Add the reversed word to the new sentence
+    #new_sentence += word.reverse
+    #new_sentence += " "
+  #  #binding.pry
+  #end
+  array.collect do |word| word.reverse
+    #new_sentence = array.reverse.join(" ")
+    new_sentence += word.reverse
+    new_sentence += " "
+  end
+  new_sentence
+  #puts new_sentence
+end
+
+#reverse_each_word("Lets go get icecream")
